dd962a41f3aec12134ba273c028817ab881885a4
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-22 17:29:25 -0300

    updated patterns to cover img and src attrs

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     jade: [[
-        /src=['"]([^"']+(?:png|gif|jpe?g|css|js))["']/img,
+        /src=['"]([^"']+\.(?:png|gif|jpe?g|css|js))["']/img,
         'Update the Jade with src attributes'
     ], [
-        /href=['"]([^"']+(?:png|gif|jpe?g|css|js))["']/img,
+        /href=['"]([^"']+\.(?:png|gif|jpe?g|css|js))["']/img,
         'Update the Jade with href attributes'
     ], [
         /url\(\s*['"]([^"']+)["']\s*\)/img,
         'Update the Jade with background imgs, case there is some inline style'
     ]],
     js: [[
-        /["']([^:"']+\.(?:png|gif|jpe?g))["']/img,
-        'Update JavaScript with images in strings'
+        /["']([^:"']+\.(?:png|gif|jpe?g|css|js))["']/img,
+        'Update JavaScript with assets in strings'
     ]]
 };
